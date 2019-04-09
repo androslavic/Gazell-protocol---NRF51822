d1f289ead8ee94b3b95a07b897dd9df07db4e5e5
androslavic <32939201+androslavic@users.noreply.github.com>
2019-04-09 10:11:49 +0200

    Add files via upload
    Established 2-way communication between host and device.

---
+++ include/gzll.h
@@ -0,0 +1,14 @@
+#ifndef GZLL_H__
+#define GZLL_H__
+
+#include "nrf51.h"
+#include "nrf_gzll.h"
+#include "nrf_gzll_constants.h"
+#include "uart.h"
+
+#endif 
+
+	
+	void gzll_init_host (void); 
+	void gzll_init_device (void); 
+
---
+++ include/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "nrf.h"                        // Device header
+#include "RTE_Components.h"             // Component selection
+#include "system_nrf51.h"               // NordicSemiconductor::Device:Startup
+#include "startup_config.h"             // NordicSemiconductor::Device:StartupConfig
+#include "uart.h"
+#include "clock.h"
+#include "gzll.h"
+
