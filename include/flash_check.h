ae08373f75e59eef0f7e4888e87272b521f0f309
androslavic <32939201+androslavic@users.noreply.github.com>
2019-05-23 12:38:39 +0200

    Add files via upload
    Work in progress.
    
    Implemented basic reading from and writing to flash memory.

---
+++ include/flash_check.h
@@ -0,0 +1,17 @@
+#ifndef FLASH_H__
+#define FLASH_H__
+
+#include "nrf51.h"
+#include "sendData.h"
+#include "ringbuf.h"
+#include "nrf_nvmc.h"
+
+
+
+void flash_check(void);
+
+
+
+#endif //FLASH_H__
+
+
